56be75a1dd4d1551332d8cf9e794f7756f765415
SamuelBajracharya <linuxsmuffproductivity@gmail.com>
2025-12-07 00:00:39 +0545

    fixed token data extraction

--- security.py
+++ security.py
@@ -38,7 +38,9 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+async def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -46,13 +48,17 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
+        phonenumber: str = payload.get("sub")
+        if phonenumber is None:
             raise credentials_exception
-        token_data = schemas.TokenData(username=username)
+        token_data = schemas.TokenData(phonenumber=phonenumber)
     except JWTError:
         raise credentials_exception
-    user = db.query(models.User).filter(models.User.username == token_data.username).first()
+    user = (
+        db.query(models.User)
+        .filter(models.User.phonenumber == token_data.phonenumber)
+        .first()
+    )
     if user is None:
         raise credentials_exception
     return user
